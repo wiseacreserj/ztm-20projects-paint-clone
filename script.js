9e551b41ee60311ed861069bda0f63d7e0c62973
Serhii Zadorozhnii <wiseacreserj@gmail.com>
2024-03-17 23:49:53 +0200

    add basic tollbar functionality

--- script.js
+++ script.js
@@ -20,49 +20,58 @@ const context = canvas.getContext("2d");
 let currentSize = 10;
 let bucketColor = "#FFFFFF";
 let currentColor = "#A51DAB";
-// let isEraser = false;
+let isEraser = false;
 // let isMouseDown = false;
 // let drawnArray = [];
 
 // Formatting Brush Size
-// function displayBrushSize() {
-
-// }
+function displayBrushSize() {
+    if (brushSlider.value < 10) {
+        brushSize.textContent = `0${brushSlider.value}`;
+    } else {
+        brushSize.textContent = brushSlider.value;
+    }
+}
 
 // Setting Brush Size
-// brushSlider.addEventListener('change', () => {
-
-// });
+brushSlider.addEventListener("change", () => {
+    currentSize = brushSlider.value;
+    displayBrushSize();
+});
 
 // Setting Brush Color
-// brushColorBtn.addEventListener('change', () => {
+brushColorBtn.addEventListener("change", () => {
+    isEraser = false;
+    currentColor = `#${brushColorBtn.value}`;
+});
 
-// });
-
-// Setting Background Color
-// bucketColorBtn.addEventListener('change', () => {
-
-// });
+//Setting Background Color
+bucketColorBtn.addEventListener("change", () => {
+    bucketColor = `#${bucketColorBtn.value}`;
+    createCanvas();
+});
 
 // // Eraser
-// eraser.addEventListener('click', () => {
-
-//   brushIcon.style.color = 'white';
-//   eraser.style.color = 'black';
-//   activeToolEl.textContent = 'Eraser';
-
-// });
+eraser.addEventListener("click", () => {
+    isEraser = true;
+    brushIcon.style.color = "white";
+    eraser.style.color = "black";
+    activeToolEl.textContent = "Eraser";
+    currentColor = bucketColor;
+    currentSize = 50;
+});
 
 // // Switch back to Brush
-// function switchToBrush() {
-//   isEraser = false;
-//   activeToolEl.textContent = 'Brush';
-//   brushIcon.style.color = 'black';
-//   eraser.style.color = 'white';
-//   currentColor = `#${brushColorBtn.value}`;
-//   currentSize = 10;
-
-// }
+function switchToBrush() {
+    isEraser = false;
+    activeToolEl.textContent = "Brush";
+    brushIcon.style.color = "black";
+    eraser.style.color = "white";
+    currentColor = `#${brushColorBtn.value}`;
+    currentSize = 10;
+    brushSlider.value = 10;
+    displayBrushSize();
+}
 
 // Create Canvas
 function createCanvas() {
@@ -194,8 +203,8 @@ canvas.addEventListener("mouseup", () => {
 //   setTimeout(switchToBrush, 1500);
 // });
 
-// // Event Listener
-// brushIcon.addEventListener('click', switchToBrush);
+// Event Listener
+brushIcon.addEventListener("click", switchToBrush);
 
 // On Load
 createCanvas();
